f66f21ba31502cf7f3711099a82101757bbfe18b
JusKendi <125484184+JusKendi@users.noreply.github.com>
2023-05-27 06:27:56 -0700

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
 /**
+* C Simple Shell - Group Project by Brenda and Nelson.
 * _myhistory - displays the history list, one command by line, preceded
 *              with line numbers, starting at 0.
 * @info: Structure containing potential arguments. Used to maintain

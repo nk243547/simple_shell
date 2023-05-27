d7a39e047e2e6ed1d3efc623063df7db5355f664
JusKendi <125484184+JusKendi@users.noreply.github.com>
2023-05-27 06:29:27 -0700

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
 /**
+* C Simple Shell - Group Project by Brenda and Nelson.
 **_strncpy - copies a string
 *@dest: the destination string to be copied to
 *@src: the source string
